26ae309871d10ac39c55b9c63db2029292322d9d
Tan How Suen <howsuen@gmail.com>
2022-05-21 18:34:36 +0800

    Add .env and redux store

--- App.js
+++ App.js
@@ -4,6 +4,8 @@ import { supabase } from './lib/supabase';
 import Auth from './components/Auth';
 import Account from './components/Account';
 import { StyleSheet, View } from 'react-native';
+import { store } from './store/store';
+import { Provider } from 'react-redux';
 
 const App = () => {
   const [session, setSession] = useState(null)
@@ -17,9 +19,11 @@ const App = () => {
   }, [])
 
   return (
-    <View style={styles.container}>
-      {session && session.user ? <Account key={session.user.id} session={session} /> : <Auth />}
-    </View>
+    <Provider store={store}>
+      <View style={styles.container}>
+        {session && session.user ? <Account key={session.user.id} session={session} /> : <Auth />}
+      </View>
+    </Provider>
   )
 }
 

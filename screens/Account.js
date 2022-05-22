5a4271ef5d2c6c9245af3fc4c1c8329dd832704c
Jordan Soh <17535785+jorrdansoh@users.noreply.github.com>
2022-05-22 22:47:39 +0800

    Upated Auth and Account screens

--- screens/Account.js
+++ screens/Account.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { supabase } from "../lib/supabase";
 import { View, Alert, TouchableOpacity } from "react-native";
-import { Button, Input, Text } from "react-native-elements";
+import { Image, Input, Text } from "react-native-elements";
 import styles from "./Account.style";
 import "react-native-url-polyfill/auto";
 
@@ -69,29 +69,42 @@ const Account = ({ session }) => {
 
   return (
     <View style={styles.container}>
-      <View style={[styles.verticallySpaced, styles.mt20]}>
-        <Input label="Email" value={session?.user?.email} disabled />
-      </View>
-      <View style={styles.verticallySpaced}>
-        <Input
-          label="Username"
-          value={username || ""}
-          onChangeText={(text) => setUsername(text)}
+      <View style={styles.avatarContainer}>
+        <Image
+          style={styles.avatar}
+          source={require('../assets/default-avatar.png')}
         />
       </View>
+      <View style={styles.formContainer}>
+        <View style={styles.verticallySpaced}>
+          <Input 
+            style={styles.textInput}
+            label="Email" 
+            value={session?.user?.email} disabled 
+          />
+        </View>
+        <View style={styles.verticallySpaced}>
+          <Input
+            style={styles.textInput}
+            label="Username"
+            value={username || ""}
+            onChangeText={(text) => setUsername(text)}
+          />
+        </View>
 
-      <TouchableOpacity 
-        style={[styles.button, styles.mt20]}
-        disabled={loading}
-        onPress={() => updateProfile({ username, avatar_url })}>
-        <Text style={styles.buttonText}>{loading ? "Loading ..." : "Update"}</Text>
-      </TouchableOpacity>
+        <TouchableOpacity 
+          style={styles.button}
+          disabled={loading}
+          onPress={() => updateProfile({ username, avatar_url })}>
+          <Text style={styles.buttonText}>{loading ? "Loading ..." : "Update"}</Text>
+        </TouchableOpacity>
 
-      <TouchableOpacity 
-        style={[styles.button]}
-        onPress={() => supabase.auth.signOut()}>
-        <Text style={styles.buttonText}>Sign Out</Text>
-      </TouchableOpacity>
+        <TouchableOpacity 
+          style={styles.button}
+          onPress={() => supabase.auth.signOut()}>
+          <Text style={styles.buttonText}>Sign Out</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
--- screens/Account.style.js
+++ screens/Account.style.js
@@ -1,31 +1,45 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, DarkTheme } from "react-native";
 
 export default StyleSheet.create({
   container: {
     padding:15,
     justifyContent:"center",
-    flex:1
+    flex:1,
+    theme:{DarkTheme},
+  },
+  avatarContainer: {
+    justifyContent:"flex-end",
+    alignItems:"center",
+    flex:1,
+  },
+  avatar: {
+    height:200,
+    width:200,
+  },
+  formContainer: {
+    marginTop:50,
+    flex:1.5,
   },
   verticallySpaced: {
     paddingTop:5,
     paddingBottom:5,
     alignSelf:"stretch",
   },
-  mt20: {
-    marginTop:20,
+  textInput: {
+    color:"white",
   },
   button: {
     marginRight:20,
     marginLeft:20,
-    marginTop:15,
+    marginTop:20,
     paddingTop:10,
     paddingBottom:10,
-    backgroundColor:"#00fa9a",
+    backgroundColor:"mediumspringgreen",
     borderRadius:5,
   },
   buttonText: {
     color:"black",
-    fontSize:18,
+    fontSize:17,
     textAlign:"center",
   },
 });

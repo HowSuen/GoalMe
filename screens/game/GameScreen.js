ff8cf97b20761d6dc357a31e1f25107440d407a3
Tan How Suen <howsuen@gmail.com>
2022-06-13 00:05:38 +0800

    Created table to store levels and experiences

--- screens/game/GameScreen.js
+++ screens/game/GameScreen.js
@@ -1,22 +1,23 @@
-import React, { useState } from "react";
-import { View } from "react-native";
-import SavedAvatar from "../../components/game/SavedAvatar";
+import React, { useState, useEffect } from "react";
+import { View, Alert } from "react-native";
+import supabase from "../../lib/supabase";
+import { useIsFocused } from "@react-navigation/native";
 import styles from "./GameScreen.style";
+import SavedAvatar from "../../components/game/SavedAvatar";
 import LevelBar from "../../components/game/LevelBar";
 
 const GameScreen = ({ navigation, session }) => {
+  const [totalXp, setTotalXp] = useState(0);
+  const [wisdomXp, setWisdomXp] = useState(0);
+  const [strengthXp, setStrengthXp] = useState(0);
+  const [moneyXp, setMoneyXp] = useState(0);
 
-  const [generalXp, setGeneralXp] = useState(0);
-  const [wisdom, setWisdom] = useState(0);
-  const [strength, setStrength] = useState(0);
-  const [money, setMoney] = useState(0);
+  const [totalLvl, setTotalLvl] = useState(0);
+  const [strengthLvl, setStrengthLvl] = useState(0);
+  const [wisdomLvl, setWisdomLvl] = useState(0);
+  const [moneyLvl, setMoneyLvl] = useState(0);
 
-  const generalLvl = 0;
-  const strengthLvl = 0;
-  const wisdomLvl = 0;
-  const moneyLvl = 0;
-
-  const generalProgress = 0.3;
+  const totalProgress = 0.3;
   const strengthProgress = 0.4;
   const wisdomProgress = 0.5;
   const moneyProgress = 0.6;
@@ -24,11 +25,47 @@ const GameScreen = ({ navigation, session }) => {
   /**
    * xp / xp_at_that_level = level, remainder leftover_xp
    * progress = leftover_xp / xp_at_new_level
-   * 
+   *
    * XP PER LEVEL = (LEVEL / X) ^ Y
    * X = 0.07      Lower X, More XP required per level
    * Y = 2         Higher Y, Larger gaps between levels
    */
+
+  const isFocused = useIsFocused();
+
+  useEffect(() => {
+    if (session) getProfile();
+  }, [session, isFocused]);
+
+  const getProfile = async () => {
+    try {
+      const user = supabase.auth.user();
+      if (!user) throw new Error("No user on the session!");
+
+      let { data, error, status } = await supabase
+        .from("experience")
+        .select()
+        .eq("id", user.id)
+        .single();
+
+      if (error && status !== 406) {
+        throw error;
+      }
+
+      if (data) {
+        setTotalXp(data.totalXP);
+        setTotalLvl(data.totalLvl);
+        setWisdomXp(data.wisdomXP);
+        setWisdomLvl(data.wisdomLvl);
+        setStrengthXp(data.strengthXP);
+        setStrengthLvl(data.strengthLvl);
+        setMoneyXp(data.moneyXP);
+        setMoneyLvl(data.moneyLvl);
+      }
+    } catch (error) {
+      Alert.alert(error.message);
+    }
+  };
 
   return (
     <View style={styles.container}>
@@ -39,8 +76,8 @@ const GameScreen = ({ navigation, session }) => {
         <LevelBar
           type="LEVEL"
           color="green"
-          level={generalLvl}
-          progress={generalProgress}
+          level={totalLvl}
+          progress={totalProgress}
         />
         <LevelBar
           type="WISDOM"

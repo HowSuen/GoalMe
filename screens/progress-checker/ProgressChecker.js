42c333acf61f8a0a7f5a135459978c35c43593a8
Jordan Soh <17535785+jorrdansoh@users.noreply.github.com>
2022-06-20 22:56:18 +0800

    Update ProgressChecker.js

--- screens/progress-checker/ProgressChecker.js
+++ screens/progress-checker/ProgressChecker.js
@@ -7,23 +7,19 @@ import supabase from "../../lib/supabase";
 export default ProgressChecker = () => {
   const user = supabase.auth.user();
   const isFocused = useIsFocused();
-  //   const [dataIsFinished, setDataIsFinished] = useState(false);
-  //   const experience = [{ thing: "me", number: 1 }];
+  const [experience, setExperience] = useState(null);
 
   useEffect(() => {
-    // getExperience().then(() => console.log("hi"));
+    getExperience().then((exp) => setExperience(exp));
   }, [isFocused]);
 
   const getExperience = async () => {
-    // setDataIsFinished(false);
-    // let promise;
-
     try {
       if (!user) throw new Error("No user on the session!");
 
       let { data, error, status } = await supabase
         .from("experience")
-        .select("*")
+        .select("completed, completedAcad, completedFit, completedFinance")
         .match({ id: user.id })
         .single();
 
@@ -31,26 +27,24 @@ export default ProgressChecker = () => {
         throw error;
       }
 
-      let experience = [];
+      let exp = [];
       Object.entries(data).forEach(([key, value] = entry) => {
-        experience.push({ thing: key, number: value });
+        exp.push({ thing: key, number: value });
       });
-      return experience;
-      //   promise = data;
+      
+      return exp;
     } catch (error) {
       Alert.alert(error.message);
     }
-    // setDataIsFinished(true);
-    // return promise;
   };
 
-  const experience = getExperience();
-  
   return (
     <View style={styles.container}>
-      <VictoryChart width={350} theme={VictoryTheme.material}>
-        <VictoryBar data={experience} x="thing" y="number" />
-      </VictoryChart>
+      {experience && (
+        <VictoryChart width={400} theme={VictoryTheme.material}>
+          <VictoryBar data={experience} x="thing" y="number" />
+        </VictoryChart>
+      )}
     </View>
   );
 };
